49447dbd1eccc3c9d557b8dbc4ccefb9f7a35a6d
flaviusone <flaviusone@yahoo.com>
2013-04-27 16:31:28 +0300

    Scos cod duplicat din Longest_way + metode de get element din board

--- src/Solution.java
+++ src/Solution.java
@@ -8,133 +8,111 @@ public class Solution {
 	/* Head ends here */
 	static void Longest_way_bot(String player, int[] pos, String[] board) {
 		int counter1 = 0, counter2 = 0, counter3 = 0, counter4 = 0, i;
+		int r_x = pos[ 0 ], r_y = pos[ 1 ], g_x = pos[ 2 ], g_y = pos[ 3 ];
+		char aux;
 		if (player.equals("r")) {
 			/* Search Down */
 			i = 1;
-			while (board[ pos[ 0 ] + i ].charAt(pos[ 1 ]) != '#'
-					&& board[ pos[ 0 ] + i ].charAt(pos[ 1 ]) != 'r'
-					&& board[ pos[ 0 ] + i ].charAt(pos[ 1 ]) != 'g') {
+			aux = getCharAtXY(board, r_x + i, r_y);
+			while (aux != '#' && aux != 'r' && aux != 'g') {
 				counter1++;
 				i++;
+				aux = getCharAtXY(board, r_x + i, r_y);
 			}
 			/* Search Up */
 			i = 1;
-			while (board[ pos[ 0 ] - i ].charAt(pos[ 1 ]) != '#'
-					&& board[ pos[ 0 ] - i ].charAt(pos[ 1 ]) != 'r'
-					&& board[ pos[ 0 ] - i ].charAt(pos[ 1 ]) != 'g') {
+			aux = getCharAtXY(board, r_x - i, r_y);
+			while (aux != '#' && aux != 'r' && aux != 'g') {
 				counter2++;
 				i++;
+				aux = getCharAtXY(board, r_x - i, r_y);
 			}
 			/* Search Right */
 			i = 1;
-			while (board[ pos[ 0 ] ].charAt(pos[ 1 ] + i) != '#'
-					&& board[ pos[ 0 ] ].charAt(pos[ 1 ] + i) != 'r'
-					&& board[ pos[ 0 ] ].charAt(pos[ 1 ] + i) != 'g') {
+			aux = getCharAtXY(board, r_x - i, r_y + i);
+			while (aux != '#' && aux != 'r' && aux != 'g') {
 				counter3++;
 				i++;
+				aux = getCharAtXY(board, r_x - i, r_y + i);
 			}
 			/* Search Left */
 			i = 1;
-			while (board[ pos[ 0 ] ].charAt(pos[ 1 ] - i) != '#'
-					&& board[ pos[ 0 ] ].charAt(pos[ 1 ] - i) != 'r'
-					&& board[ pos[ 0 ] ].charAt(pos[ 1 ] - i) != 'g') {
+			aux = getCharAtXY(board, r_x - i, r_y - i);
+			while (aux != '#' && aux != 'r' && aux != 'g') {
 				counter4++;
 				i++;
-			}
-
-			/* See whitch way is the best one */
-			int way;
-			if (counter1 >= counter2 && counter1 >= counter3 && counter1 >= counter4) {
-				way = 0; /* Down */
-			}
-			else if (counter2 >= counter1 && counter2 >= counter3 && counter2 >= counter4)
-				way = 1; /* Up */
-			else if (counter3 >= counter2 && counter3 >= counter1 && counter3 >= counter4)
-				way = 2; /* Right */
-			else
-				way = 3; /* Left */
-
-			/* Print the best way to go */
-			switch (way) {
-			case 0:
-				System.out.println("DOWN");
-				break;
-			case 1:
-				System.out.println("UP");
-				break;
-			case 2:
-				System.out.println("RIGHT");
-				break;
-			default:
-				System.out.println("LEFT");
-				break;
+				aux = getCharAtXY(board, r_x - i, r_y - i);
 			}
 
 		}
 		else {
 			/* Search Down */
 			i = 1;
-			while (board[ pos[ 2 ] + i ].charAt(pos[ 3 ]) != '#'
-					&& board[ pos[ 2 ] + i ].charAt(pos[ 3 ]) != 'r'
-					&& board[ pos[ 2 ] + i ].charAt(pos[ 3 ]) != 'g') {
+			aux = getCharAtXY(board, g_x + i, g_y);
+			while (aux != '#' && aux != 'r' && aux != 'g') {
 				counter1++;
 				i++;
+				aux = getCharAtXY(board, g_x + i, g_y);
 			}
 			/* Search Up */
 			i = 1;
-			while (board[ pos[ 2 ] - i ].charAt(pos[ 3 ]) != '#'
-					&& board[ pos[ 2 ] - i ].charAt(pos[ 3 ]) != 'r'
-					&& board[ pos[ 2 ] - i ].charAt(pos[ 3 ]) != 'g') {
+			aux = getCharAtXY(board, g_x - i, g_y);
+			while (aux != '#' && aux != 'r' && aux != 'g') {
 				counter2++;
 				i++;
+				aux = getCharAtXY(board, g_x - i, g_y);
 			}
 			/* Search Right */
 			i = 1;
-			while (board[ pos[ 2 ] ].charAt(pos[ 3 ] + i) != '#'
-					&& board[ pos[ 2 ] ].charAt(pos[ 3 ] + i) != 'r'
-					&& board[ pos[ 2 ] ].charAt(pos[ 3 ] + i) != 'g') {
+			aux = getCharAtXY(board, g_x - i, g_y + i);
+			while (aux != '#' && aux != 'r' && aux != 'g') {
 				counter3++;
 				i++;
+				aux = getCharAtXY(board, g_x - i, g_y + i);
 			}
 			/* Search Left */
 			i = 1;
-			while (board[ pos[ 2 ] ].charAt(pos[ 3 ] - i) != '#'
-					&& board[ pos[ 2 ] ].charAt(pos[ 3 ] - i) != 'r'
-					&& board[ pos[ 2 ] ].charAt(pos[ 3 ] - i) != 'g') {
+			aux = getCharAtXY(board, g_x - i, g_y - i);
+			while (aux != '#' && aux != 'r' && aux != 'g') {
 				counter4++;
 				i++;
-			}
-
-			/* See whitch way is the best one */
-			int way;
-			if (counter1 >= counter2 && counter1 >= counter3 && counter1 >= counter4) {
-				way = 0; /* Down */
-			}
-			else if (counter2 >= counter1 && counter2 >= counter3 && counter2 >= counter4)
-				way = 1; /* Up */
-			else if (counter3 >= counter2 && counter3 >= counter1 && counter3 >= counter4)
-				way = 2; /* Right */
-			else
-				way = 3; /* Left */
-
-			/* Print the best way to go */
-			switch (way) {
-			case 0:
-				System.out.println("DOWN");
-				break;
-			case 1:
-				System.out.println("UP");
-				break;
-			case 2:
-				System.out.println("RIGHT");
-				break;
-			default:
-				System.out.println("LEFT");
-				break;
+				aux = getCharAtXY(board, g_x - i, g_y - i);
 			}
 
 		}
 
+		/* See whitch way is the best one */
+		int way;
+		if (counter1 >= counter2 && counter1 >= counter3 && counter1 >= counter4) {
+			way = 0; /* Down */
+		}
+		else if (counter2 >= counter1 && counter2 >= counter3 && counter2 >= counter4)
+			way = 1; /* Up */
+		else if (counter3 >= counter2 && counter3 >= counter1 && counter3 >= counter4)
+			way = 2; /* Right */
+		else
+			way = 3; /* Left */
+
+		/* Print the best way to go */
+		switch (way) {
+		case 0:
+			System.out.println("DOWN");
+			break;
+		case 1:
+			System.out.println("UP");
+			break;
+		case 2:
+			System.out.println("RIGHT");
+			break;
+		default:
+			System.out.println("LEFT");
+			break;
+		}
+
+	}
+
+	static char getCharAtXY(String[] board, int x, int y) {
+		return board[ x ].charAt(y);
 	}
 
 	static void nextMove(String player, int[] pos, String[] board) {
